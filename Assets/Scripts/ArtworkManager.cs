e7c355cc31776275e28bf70adf9bba0c7cc91f7b
limi <limi@cip.ifi.lmu.de>
2024-09-16 22:35:15 +0200

    flowerarragment page with vase and flower display bar (flower selection/display should be fixed)

--- Assets/Scripts/ArtworkManager.cs
+++ Assets/Scripts/ArtworkManager.cs
@@ -1,27 +1,33 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ArtworkManager : MonoBehaviour
 {
-    public GameObject enlargedImageDisplay;   
-    public RectTransform scrollViewContent;   
-    public Transform flowerDisplayBar;        
-    public GameObject flowerButtonPrefab;     
-    public GameObject fullScreen3DView;       
-    public Transform flower3DViewTransform;   
-    public Button close3DViewButton;         
-    private Image enlargedImageComponent;     
-    private GameObject current3DFlower;    
-    private GameObject currentArtwork;      
+    public GameObject enlargedImageDisplay;        
+    public RectTransform scrollViewContent;        
+    public Transform flowerDisplayBar;             
+    public GameObject flowerButtonPrefab;          // 花朵按钮的预制体
+    public GameObject fullScreen3DView;            
+    public Transform flower3DViewTransform;        // 放置3D花朵模型的容器
+    public Button close3DViewButton;              
+    public Button endFlowerSelectionButton;       
+    public GameObject flowerArrangementArea;       
+    public Transform upper3DFlowerTransform;       // 3D花朵显示在页面顶部的位置
+    private Image enlargedImageComponent;          
+    private GameObject current3DFlower;           
+    private GameObject currentArtwork;             
+    private bool isFlowerArrangementPhase = false; 
 
     void Start()
     {
         enlargedImageComponent = enlargedImageDisplay.GetComponent<Image>();
 
+        // 为 ScrollView 中的画作绑定点击事件
         foreach (Transform imageTransform in scrollViewContent)
         {
             Button imageButton = imageTransform.GetComponent<Button>();
-            Artwork artwork = imageTransform.GetComponent<Artwork>();  
+            Artwork artwork = imageTransform.GetComponent<Artwork>();
             if (imageButton != null && artwork != null)
             {
                 imageButton.onClick.AddListener(() => OnArtworkClick(imageButton, artwork));
@@ -32,10 +38,15 @@ public class ArtworkManager : MonoBehaviour
             }
         }
 
+        // 初始化插花区域和全屏3D视图，初始时隐藏
         fullScreen3DView.SetActive(false);
+        flowerArrangementArea.SetActive(false);
+
+        endFlowerSelectionButton.onClick.AddListener(OnEndFlowerSelection);
+
         if (close3DViewButton != null)
         {
-            close3DViewButton.gameObject.SetActive(false);  
+            close3DViewButton.gameObject.SetActive(false);
             close3DViewButton.onClick.AddListener(HideFullScreen3DView);
         }
         else
@@ -44,6 +55,7 @@ public class ArtworkManager : MonoBehaviour
         }
     }
 
+    // 点击画作时显示画作并绑定花朵区域点击事件
     void OnArtworkClick(Button clickedButton, Artwork artwork)
     {
         Image clickedImage = clickedButton.GetComponent<Image>();
@@ -52,10 +64,7 @@ public class ArtworkManager : MonoBehaviour
         {
             enlargedImageComponent.sprite = clickedImage.sprite;
 
-            float originalWidth = clickedImage.sprite.texture.width;
-            float originalHeight = clickedImage.sprite.texture.height;
-            float aspectRatio = originalWidth / originalHeight;
-
+            float aspectRatio = (float)clickedImage.sprite.texture.width / clickedImage.sprite.texture.height;
             float displayHeight = 500f;
             float displayWidth = displayHeight * aspectRatio;
 
@@ -64,28 +73,29 @@ public class ArtworkManager : MonoBehaviour
 
             enlargedImageDisplay.SetActive(true);
 
+            // 禁用之前画作的花朵区域
             if (currentArtwork != null)
             {
                 DisableFlowerAreas(currentArtwork);
             }
 
             currentArtwork = clickedButton.gameObject;
-
             EnableFlowerAreas(artwork);
 
-            foreach (RectTransform flowerArea in artwork.flowerAreas)
+            // 为每个花朵区域绑定点击事件
+            for (int i = 0; i < artwork.flowerAreas.Length; i++)
             {
+                RectTransform flowerArea = artwork.flowerAreas[i];
                 Button flowerButton = flowerArea.GetComponent<Button>();
                 if (flowerButton == null)
                 {
-                    flowerButton = flowerArea.gameObject.AddComponent<Button>(); 
+                    flowerButton = flowerArea.gameObject.AddComponent<Button>();
                 }
 
                 flowerButton.onClick.RemoveAllListeners();
-
                 flowerButton.gameObject.SetActive(true);
 
-                int index = System.Array.IndexOf(artwork.flowerAreas, flowerArea);  
+                int index = i; 
                 flowerButton.onClick.AddListener(() => OnFlowerAreaClick(artwork.flowerSprites[index], artwork.flower3DModels[index]));
             }
         }
@@ -95,11 +105,18 @@ public class ArtworkManager : MonoBehaviour
         }
     }
 
+void OnFlowerAreaClick(Sprite flowerSprite, GameObject flower3DModel)
+{
+    Show2DFlower(flowerSprite);
+
+    ShowFullScreen3DFlower(flower3DModel);
+}
+
     void EnableFlowerAreas(Artwork artwork)
     {
         foreach (RectTransform flowerArea in artwork.flowerAreas)
         {
-            flowerArea.gameObject.SetActive(true); 
+            flowerArea.gameObject.SetActive(true);
         }
     }
 
@@ -110,17 +127,17 @@ public class ArtworkManager : MonoBehaviour
         {
             foreach (RectTransform flowerArea in previousArtworkScript.flowerAreas)
             {
-                flowerArea.gameObject.SetActive(false); 
+                flowerArea.gameObject.SetActive(false);
             }
         }
     }
 
-
-    void OnFlowerAreaClick(Sprite flowerSprite, GameObject flower3DModel)
+    void OnFlowerBarClick(GameObject flower3DModel)
     {
-        Show2DFlower(flowerSprite);
-
-        ShowFullScreen3DFlower(flower3DModel);
+        if (isFlowerArrangementPhase)
+        {
+            Show3DFlowerAtTop(flower3DModel);
+        }
     }
 
     void Show2DFlower(Sprite flowerSprite)
@@ -140,7 +157,6 @@ public class ArtworkManager : MonoBehaviour
         }
 
         fullScreen3DView.SetActive(true);
-
         close3DViewButton.gameObject.SetActive(true);
 
         current3DFlower = Instantiate(flower3DModel, flower3DViewTransform);
@@ -169,10 +185,85 @@ public class ArtworkManager : MonoBehaviour
         }
 
         fullScreen3DView.SetActive(false);
-
         close3DViewButton.gameObject.SetActive(false);
+        enlargedImageDisplay.SetActive(true);
 
-        enlargedImageDisplay.SetActive(true);  
         Debug.Log("Closed full-screen 3D view, returning to flower selection");
+    }
+
+    void OnEndFlowerSelection()
+    {
+        enlargedImageDisplay.SetActive(false);
+        scrollViewContent.gameObject.SetActive(false);
+        flowerArrangementArea.SetActive(true);
+        flowerDisplayBar.gameObject.SetActive(true);
+
+        if (endFlowerSelectionButton != null)
+        {
+            endFlowerSelectionButton.gameObject.SetActive(false);
+        }
+
+        isFlowerArrangementPhase = true;
+        BindFlowerBarClickEvents();
+
+        Debug.Log("Entered flower arrangement phase");
+    }
+
+    void BindFlowerBarClickEvents()
+    {
+        foreach (Transform flowerButton in flowerDisplayBar)
+        {
+            Button button = flowerButton.GetComponent<Button>();
+            if (button != null)
+            {
+                button.onClick.RemoveAllListeners();
+                GameObject flower3DModel = Get3DModelForFlower(button);
+                button.onClick.AddListener(() => OnFlowerBarClick(flower3DModel));
+            }
+        }
+    }
+
+    GameObject Get3DModelForFlower(Button button)
+    {
+        string buttonName = button.name.Replace("(Clone)", "").Trim();
+        Debug.Log("Button name after cleaning: " + buttonName);
+
+        string path = "3DModels/" + buttonName;
+        Debug.Log("Loading 3D model from path: " + path);
+
+        GameObject flower3DModel = Resources.Load<GameObject>(path);
+
+        if (flower3DModel == null)
+        {
+            Debug.LogError("Failed to load 3D model for: " + buttonName);
+        }
+        else
+        {
+            Debug.Log("Successfully loaded 3D model for: " + buttonName);
+        }
+
+        return flower3DModel;
+    }
+    void Show3DFlowerAtTop(GameObject flower3DModel)
+    {
+        if (current3DFlower != null)
+        {
+            Destroy(current3DFlower);
+        }
+
+        current3DFlower = Instantiate(flower3DModel, upper3DFlowerTransform);
+
+        if (current3DFlower != null)
+        {
+            Debug.Log("Successfully loaded 3D model: " + current3DFlower.name);
+
+            current3DFlower.transform.localScale = Vector3.one;
+            current3DFlower.transform.localPosition = Vector3.zero;
+            current3DFlower.transform.localRotation = Quaternion.identity;
+        }
+        else
+        {
+            Debug.LogError("Failed to load 3D model");
+        }
     }
 }
